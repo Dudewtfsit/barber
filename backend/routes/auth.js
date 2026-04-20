4bd8cd1291311202698ad31083d1c558d3cf6149
badr <yourfdkgjdfk@gmail.com>
2026-04-20 10:51:21 +0100

    your message here

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 // backend/routes/auth.js
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 const pool = require('../config/db');
